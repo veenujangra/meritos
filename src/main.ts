587b7baa8bc9f55b0bcf224216c0514d845f47c1
veenujangra <veenuj01@gmail.com>
2025-10-21 17:27:44 +0530

    default page animations added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import About from './page/about'
 import Home from './page/home'
+import Page from './page'
 import './style.css'
 
 class Meritos {
@@ -14,11 +15,12 @@ class Meritos {
       this.home = new Home({
         element: document.querySelector('[data-page="home"]') as HTMLElement,
       })
-    }
-    if (document.querySelector('[data-page="about"]')) {
+    } else if (document.querySelector('[data-page="about"]')) {
       this.about = new About({
         element: document.querySelector('[data-page="about"]') as HTMLElement,
       })
+    } else {
+      new Page({ element: document.querySelector('[data-page]') as HTMLElement })
     }
   }
 
